05567a67617934212aab13d02c18c5757c3b8f56
Aahwan Hazari <74370173+AahwanHazari@users.noreply.github.com>
2026-06-18 18:23:35 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -35,3 +35,83 @@ window.addEventListener('DOMContentLoaded', () => {
     window.location.href = 'index.html';
   }
 });
+
+let fuel = 0;
+let co2 = 0;
+let delay = 0;
+let violations = 0;
+
+function animateValue(id, start, end, suffix = "") {
+
+  let current = start;
+
+  const increment = (end - start) / 25;
+
+  document.getElementById(id).classList.add("pulse");
+
+  setTimeout(() => {
+    document.getElementById(id).classList.remove("pulse");
+  }, 500);
+
+  const timer = setInterval(() => {
+
+    current += increment;
+
+    if (
+      (increment > 0 && current >= end) ||
+      (increment < 0 && current <= end)
+    ) {
+      current = end;
+      clearInterval(timer);
+    }
+
+    document.getElementById(id).innerHTML =
+      (suffix === " L" || suffix === " kg")
+        ? current.toFixed(2) + suffix
+        : Math.round(current) + suffix;
+
+  }, 20);
+
+}
+
+window.showReward = function() {
+
+  let oldFuel = fuel;
+  let oldCo2 = co2;
+  let oldDelay = delay;
+  let oldViolations = violations;
+
+  fuel += 0.05;
+  co2 += 0.12;
+  delay += 3;
+  violations += 1;
+
+  animateValue("fuel", oldFuel, fuel, " L");
+  animateValue("co2", oldCo2, co2, " kg");
+  animateValue("delay", oldDelay, delay, " min");
+  animateValue("violations", oldViolations, violations);
+
+  document.getElementById("rewardMessage").style.display =
+    "block";
+
+  /* Citizen Badge */
+
+  const badge =
+  document.getElementById("citizenBadge");
+
+  if (violations >= 10) {
+
+    badge.innerHTML =
+    "🥇 Urban Mobility Champion";
+
+  } else if (violations >= 5) {
+
+    badge.innerHTML =
+    "🥈 Smart City Supporter";
+
+  } else {
+
+    badge.innerHTML =
+    "🥉 Responsible Citizen";
+  }
+  };
